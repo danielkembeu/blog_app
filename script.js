6c5dd0092e582c3abea12d225b74b4f4149495c1
danielkembeu <kembeudaniel2@gmail.com>
2024-10-23 12:42:13 +0100

    Added a new function into script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 let welcomeMessage = "Hey👋!";
 
 alert(`${welcomeMessage} + This is my Blog app.`);
+
+
+// First function to console.log something quickly.
+function log(message) {
+    console.log(...message);
+}
+
+log("This is my Blog app");
